d0a2b815c409d1eafd0ad52b5a2338a86c01e3d4
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-05-16 23:35:04 +0300

    Added app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  env = require('./server/config/env.js'),
+  router = require('./server/router/index'),
+  Sequelize = require('sequelize'),
+  db = {};
+
+const app = express();
+const PORT = env.PORT;
+ 
+const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
+    host: env.DATABASE_HOST,
+    port: env.DATABASE_PORT,
+    dialect: env.DATABASE_DIALECT
+  });
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require('./server/models/users.js')(sequelize, Sequelize);
+db.profiles = require('./server/models/profiles.js')(sequelize, Sequelize);
+
+module.exports = db;
+   
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+   res.header('Content-Type', 'application/json');
+   next();
+  });
+
+router(app, db);
+
+db.sequelize.sync().then(() => {
+   app.listen(PORT, () => {
+     console.log('Express listening on port:', PORT);
+   });
+
+});
