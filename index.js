23831bf0d961b10e7e3f78af3328cf0db9d5c6ea
Ivan Shyika <i.shyyka@ukr.net>
2017-05-17 04:36:54 +0300

    Added server

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const app = require('./server/app');
+
+const port = parseInt(process.env.PORT, 10) || 8080;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`The server is running at localhost:${port}`);
+});
+
