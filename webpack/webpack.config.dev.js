689f7a3dd0d8550c00099f9c9fa84cb4a6ec4be0
yuriy <borodatuu@gmail.com>
2017-05-21 22:46:51 +0300

    create webpack configuration

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+
+module.exports = {
+  entry: './server/app.js',
+
+  output: {
+    path: path.resolve('build'),
+    filename: "bundle.js"
+  },
+ 
+  target: 'node',
+  externals: [nodeExternals()],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      { 
+        test: /\.json$/, 
+        loader: "json-loader"
+      }
+    ]
+  },
+
+  plugins: [
+  new webpack.DefinePlugin({
+        'process.VERSION': require('package.json').version,
+        'process.browser': true
+      }),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
+  ],
+
+  devtool: 'source-map'
+  
+}
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,23 @@
+const WebpackStripLoader = require('strip-loader');
+const devConfig = require('./webpack.config.dev.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const stripLoader = {
+  test: [/\.js$/],
+  exclude: /node_modules/,
+  loader: WebpackStripLoader.loader('console.log')
+}
+
+devConfig.module.rules.push(stripLoader);
+
+module.exports = {
+  plugins: [
+    new CleanWebpackPlugin(['build'], {
+      root: './webpack.config.dev.js',
+      verbose: true,
+      dry: false
+    })
+  ]
+};
+
+module.exports = devConfig;
