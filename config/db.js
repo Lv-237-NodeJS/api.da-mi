1c399805dcac2d04ee2e28ecf98026cd6cef6bce
Ivan Shyika <i.shyyka@ukr.net>
2017-05-21 19:34:15 +0300

    Added server and config

---
+++ config/db.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const db_example = require(`${__dirname}/./db_example.json`)[env];
+const config = require(`${__dirname}/./config.json`)[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  sequelize = new Sequelize(
+    db_example.database, db_example.username, db_example.password, db_example
+  ) 
+}
+
+fs
+  .readdirSync(path.resolve('./server/models'))
+  .filter((file) =>
+    (file.indexOf('.') !== 0) &&
+    (file !== basename) &&
+    (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(path.resolve('./server/models'), file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
