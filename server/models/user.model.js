07b7db467828fe465186ccf95a2a4989e198c052
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-05-23 08:53:42 -0700

    Deleted index.js and added model.user.js with .jscsrc rules

---
+++ server/models/user.model.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    profile_id: {
+      type: DataTypes.INTEGER
+    },
+    is_activate: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    status_state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    timestamps: false,
+    underscored: true
+  });
+
+  return User;
+};
