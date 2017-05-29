b42b91e6dac33686f362e9106ce1771c6291d17e
yuliannakorol <yuliannakorol@gmail.com>
2017-05-29 15:08:14 +0300

    updated controller

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -21,14 +21,16 @@ module.exports = {
         return res.status(404).send({message: 'Profile Not Found',
         });
       }
-      return profile.updateAttributes({
-        first_name: req.body.first_name || profile.first_name,
-        last_name: req.body.last_name || profile.last_name,
-        birth_date: req.body.birth_date || profile.birth_date,
-        address: req.body.address || profile.address,
-        city: req.body.city || profile.city,
-        country: req.body.country || profile.country,
-      })
+      let updateProfile = Object.assign({}, req.body);
+      return profile.updateAttributes(updateProfile || profile)
+      // return profile.updateAttributes({
+      //   first_name: req.body.first_name || profile.first_name,
+      //   last_name: req.body.last_name || profile.last_name,
+      //   birth_date: req.body.birth_date || profile.birth_date,
+      //   address: req.body.address || profile.address,
+      //   city: req.body.city || profile.city,
+      //   country: req.body.country || profile.country,
+      // })
       .then(() => res.status(200).send(profile))
       .catch(error => res.status(400).send(error));
     })
