d230068d36ffef66e9392a081f9d6cdf7be815b8
yuliannakorol <yuliannakorol@gmail.com>
2017-05-29 15:13:19 +0300

    updated controller

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -23,14 +23,6 @@ module.exports = {
       }
       let updateProfile = Object.assign({}, req.body);
       return profile.updateAttributes(updateProfile || profile)
-      // return profile.updateAttributes({
-      //   first_name: req.body.first_name || profile.first_name,
-      //   last_name: req.body.last_name || profile.last_name,
-      //   birth_date: req.body.birth_date || profile.birth_date,
-      //   address: req.body.address || profile.address,
-      //   city: req.body.city || profile.city,
-      //   country: req.body.country || profile.country,
-      // })
       .then(() => res.status(200).send(profile))
       .catch(error => res.status(400).send(error));
     })
