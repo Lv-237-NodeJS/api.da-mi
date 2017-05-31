86c1f67848f178dc299fd293b420a66df5fd2957
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-05-30 23:50:28 -0700

    Added some changes for user.controller and deleted unneccesary controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,74 +3,24 @@
 const User = require('../../config/db').Users;
 const Profile = require('../../config/db').Profiles;
 const bcrypt = require('bcrypt');
+const rounds = 8;
 
 module.exports = {
   create(req, res) {
     let assignUser = Object.assign({}, req.body);
-    Profile.create();
-    return User
-      .create(assignUser)
-      .then((user) => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
+    let builtProfile = Profile.build();
 
-  list(req, res) {
-    return User
-      .findAll({
-        order: [
-          ['created_at', 'ASC']
-        ],
-      })
-      .then(users => res.status(200).send(users))
-      .catch(error => res.status(400).send(error));
-  },
+    Profile.create(builtProfile).then(result => {
+      let createdProfile = result;
+      assignUser.profile_id = createdProfile.dataValues.id;
 
-  retrieve(req, res) {
-    return User
-      .findById(req.params.id)
-      .then(user => {
-        if (!user) {
-          return res.status(404).send({
-            message: 'User has not found. Please ty again!'
-          });
-        }
-        return res.status(200).send(user);
-      })
-      .catch(error => res.status(400).send(error));
-  },
+      User.create(assignUser)
+        .then(user => res.status(201).send(user))
+        .catch(error => res.status(400)).send(error);
+    });
 
-  update(req, res) {
-    return User
-      .findById(req.params.id)
-      .then(user => {
-        if (!user) {
-          return res.status(404).send({
-            message: 'User has not found. Please ty again!'
-          });
-        }
-        let assignUser = Object.assign({}, req.body);
-        return user
-          .update(assignUser || user)
-          .then(user => res.status(200).send(user))
-          .catch(error => res.status(400).send(error));
-      })
-      .catch(error => res.status(400).send(error));
-  },
-
-  destroy(req, res) {
-    return User
-      .findById(req.params.id)
-      .then(user => {
-        if (!user) {
-          return res.status(404).send({
-            message: 'User has not found. Please try again!'
-          });
-        }
-        return user
-          .destroy()
-          .then(user => res.status(204).send(user))
-          .catch(error => res.status(404).send(error));
-      })
-      .catch(error => res.status(404).send(error));
+    User.hook('afterValidate', (user, options) => {
+      assignUser.password = bcrypt.hashSync(assignUser.password, rounds);
+    });
   }
 };
