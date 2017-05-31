473a02960a016d75cce9b128a6d52ef555cb55ab
yuliannakorol <yuliannakorol@gmail.com>
2017-05-31 15:59:13 +0300

    refactored profile controller

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -4,17 +4,19 @@ const Profile = require('../../config/db').Profile;
 
 module.exports = {
   retrieve(req, res) {
-    return Profile.findById(req.params.id)
+    Profile.findById(req.params.id)
     .then(profile => {
       if (!profile) {
         return res.status(404).send({message: 'Profile Not Found'});
       }
       return res.status(200).send(profile);
     })
-    .catch(error => res.status(400).send(error));
+    .catch(error => {
+        return res.status(400).send(error);
+      });
   },
   update(req, res) {
-    return Profile.findById(req.params.id)
+    Profile.findById(req.params.id)
     .then(profile => {
       if (!profile) {
         return res.status(404).send({message: 'Profile Not Found',
@@ -25,6 +27,8 @@ module.exports = {
       .then(() => res.status(200).send(profile))
       .catch(error => res.status(400).send(error));
     })
-    .catch(error => res.status(555).send(error));
+    .catch(error => {
+        return res.status(555).send(error);
+      });
   }
 };
