784c437ae49e3bfb6531916a7bf6aa5752fed2e2
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-05-31 00:02:20 -0700

    Added changes for user seed file

--- server/seeders/user.seed.js
+++ server/seeders/user.seed.js
@@ -1,23 +1,24 @@
 'use strict';
 
 const bcrypt = require('bcrypt');
+const rounds = 8;
 
 module.exports = {
   up: (queryInterface, Sequelize) =>
     queryInterface.bulkInsert('Users', [
       {
         email: 'ivan.yarymovych@gmail.com',
-        password: bcrypt.hashSync('password', 8),
+        password: bcrypt.hashSync('password', rounds),
         is_activate: true,
-        created_at: '2017-05-25 15:53:13.815+03',
-        updated_at: '2017-05-25 15:53:13.815+03'
+        createdAt: '2017-05-25 15:53:13.815+03',
+        updatedAt: '2017-05-25 15:53:13.815+03'
       },
       {
         email: 'roma@gmail.com',
-        password: bcrypt.hashSync('13452', 8),
+        password: bcrypt.hashSync('13452', rounds),
         is_activate: true,
-        created_at: '2017-05-25 15:53:13.815+03',
-        updated_at: '2017-05-25 15:53:13.815+03'
+        createdAt: '2017-05-25 15:53:13.815+03',
+        updatedAt: '2017-05-25 15:53:13.815+03'
       }
     ]),
   down: (queryInterface, Sequelize) =>
