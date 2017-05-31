73114d78bdd836047cd2134267560b45a7977e57
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-01 00:38:52 +0300

    Added profile_id into user seed and changed passwords for two rows

--- server/seeders/user.seed.js
+++ server/seeders/user.seed.js
@@ -8,15 +8,17 @@ module.exports = {
     queryInterface.bulkInsert('Users', [
       {
         email: 'ivan.yarymovych@gmail.com',
-        password: bcrypt.hashSync('password', rounds),
+        password: bcrypt.hashSync('YaRymov!11', rounds),
         is_activate: true,
+        profile_id: 1,
         createdAt: '2017-05-25 15:53:13.815+03',
         updatedAt: '2017-05-25 15:53:13.815+03'
       },
       {
         email: 'roma@gmail.com',
-        password: bcrypt.hashSync('13452', rounds),
+        password: bcrypt.hashSync('112345Qwerty%', rounds),
         is_activate: true,
+        profile_id: 2,
         createdAt: '2017-05-25 15:53:13.815+03',
         updatedAt: '2017-05-25 15:53:13.815+03'
       }
