5f1f1939defdb8e1756f8e1be0660da621e59fe0
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-05-31 23:27:37 +0300

    Added routes

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const userController = require('../controllers/user.controller');
+
+module.exports = app => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Da - Mi project !!!'
+  }));
+
+    app.post('/api/users', userController.create);
+};
