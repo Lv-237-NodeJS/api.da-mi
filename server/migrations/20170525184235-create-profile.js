622e061a70fcb4821310b7618c610d7d923624bf
yuliannakorol <yuliannakorol@gmail.com>
2017-05-31 10:25:00 +0300

    removed comments

--- server/migrations/20170525184235-create-profile.js
+++ server/migrations/20170525184235-create-profile.js
@@ -34,13 +34,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        // defaultValue: Sequelize.fn('now')
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        // defaultValue: Sequelize.fn('now')
+        type: Sequelize.DATE
       }
     });
   },
