9ec1e23030be9af5a76a56e8f5ec9e945afbaa18
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-01 11:15:19 +0300

    Added afterValidate hook for user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const User = require('../../config/db').Users;
+const bcrypt = require('bvrypt');
+const rounds = 8;
 
 module.exports = {
   create(req, res) {
     let assignUser = Object.assign({}, req.body);
+
+    User.hook('afterValidate', (user, options) => {
+       user.password = bcrypt.hashSync(user.password, rounds);
+    });
+
     User.create(assignUser)
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400)).send(error);
