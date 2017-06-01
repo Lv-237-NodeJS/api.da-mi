d590d880f8e0630dacbc99057130c81dd4590a2a
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-01 21:42:48 +0300

    Added model of the user

---
+++ server/models/user.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports  = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+          is: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/i,
+          len: [6,20]
+      }
+    },
+    profile_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    is_activate: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    }
+  }, {
+    paranoid: false,
+    classMethods: {
+      associate: models => {
+        User.hasOne(models.Profile, {
+          foreignKey: 'profile_id',
+          onDelete: 'cascade'
+        });
+      }
+    }
+  });
+  return User;
+};
