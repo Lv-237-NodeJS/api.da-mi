cba14e8621f09ae0df6809d0c8abee9027a7f64c
Ivan Shyika <i.shyyka@ukr.net>
2017-06-02 02:35:31 +0300

    Removed invalid indentation character

--- server/helper/mailer.js
+++ server/helper/mailer.js
@@ -7,16 +7,18 @@ const fs = require('fs');
 const path = require('path');
 
 const secret = require(path.resolve('./config', 'secretkey.json'));
-let	readHTMLTemplate = function(_name, callback) {	
-    fs.readFile(path.join('./server/templates/mailer/' + _name + '.html'), {encoding: 'utf-8'}, 
-    	function (err, html) {
-	        if (err) {
-	            throw err;
-	        }
-	        callback(null, html);
-	    }
+let readHTMLTemplate = function(_name, callback) {
+    fs.readFile(path.join('./server/templates/mailer/' + _name + '.html'), {
+            encoding: 'utf-8'
+        },
+        function(err, html) {
+            if (err) {
+                throw err;
+            }
+            callback(null, html);
+        }
     );
-}
+};
 
 let transport = nodemailer.createTransport(smtpTransport({
     host: 'smtp.gmail.com',
@@ -29,38 +31,38 @@ let transport = nodemailer.createTransport(smtpTransport({
 }));
 
 module.exports = {
-	_send(_data, _template) {
-		readHTMLTemplate(_template, function(err, html) {			
-			let signupUrl = 'http://'+ _data.host + _data.route +_data.token;		
-			let template = handlebars.compile(html);
-			let replacements = {
-				firstname: _data.firstname,
-			    lastname: _data.lastname,
-			    to : _data.email,
-			    signupUrl: signupUrl,
-			    eventName: _data.eventName,
-			    date: _data.date,
-			    eventDescription: _data.eventDescription
-		    };
+    _send(_data, _template) {
+        readHTMLTemplate(_template, function(err, html) {
+            let signupUrl = 'http://' + _data.host + _data.route + _data.token;
+            let template = handlebars.compile(html);
+            let replacements = {
+                firstname: _data.firstname,
+                lastname: _data.lastname,
+                to: _data.email,
+                signupUrl: signupUrl,
+                eventName: _data.eventName,
+                date: _data.date,
+                eventDescription: _data.eventDescription
+            };
 
-		let htmlToSend = template(replacements);
+            let htmlToSend = template(replacements);
 
-		let mailOptions = {
-			from: '"Da-Mi" <no-reply@da-mi.com>',
-		    to : _data.email,
-		    subject : _data.subject,
-		    html : htmlToSend,
-		    attachments: [{
-		    	filename: _data.img,
-		    	path: ('./server/templates/mailer/img/' + _data.img),
-		    	cid: 'bonsai@kreata.ee'
-			    }]
-		     };		    
-		    transport.sendMail(mailOptions, function (error, response) {
-		        if (error) {
-		         	callback(error);
-		        }
-		    });
-		});
-	}
+            let mailOptions = {
+                from: '"Da-Mi" <no-reply@da-mi.com>',
+                to: _data.email,
+                subject: _data.subject,
+                html: htmlToSend,
+                attachments: [{
+                    filename: _data.img,
+                    path: ('./server/templates/mailer/img/' + _data.img),
+                    cid: 'bonsai@kreata.ee'
+                }]
+            };
+            transport.sendMail(mailOptions, function(error, response) {
+                if (error) {
+                    callback(error);
+                }
+            });
+        });
+    }
 }
