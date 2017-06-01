1d8e802e29d368b2a5817176c04dbb3da8d427e6
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-01 21:59:56 +0300

    Added some spaces changes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -10,14 +10,11 @@ module.exports = {
     User.hook('afterValidate', (user, options) => {
        user.password = passwordHash.generate(user.password);
     });
-
-  
+      
     User.create(assignUser)
     .then(user => res.status(201).send(user))
-    .catch(error => res.status(400).send(error));
-        
+    .catch(error => res.status(400).send(error));       
   },
-
   destroy(req, res) {
     User.findById(req.params.id)
     .then(user => {
