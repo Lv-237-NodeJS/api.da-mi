0462c64da051a9e298a592f0ccb75fa59d4bd0e2
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-06-04 18:33:46 +0300

    Added secret key to the session

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const secret = require('./../config/jwt.secretkey.json');
 const app = express();
 
 app.use(logger('dev'));
@@ -14,7 +15,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(session({
-  secret: 'supersecret',
+  secret: secret.key,
   resave: false,
   saveUninitialized: false
 }));
