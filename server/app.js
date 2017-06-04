49a74dca4c7e5c8de42cb6ad5f14d14c897ef169
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-06-04 18:45:07 +0300

    Fixed indentation

--- server/app.js
+++ server/app.js
@@ -7,10 +7,10 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
@@ -20,7 +20,7 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   let token = req.session.token;
   next();
 });
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,15 +6,15 @@ const jwt = require('jsonwebtoken');
 const secret = require('./../../config/jwt.secretkey.json');
 
 module.exports = {
-  login (req, res) {
-    User.findOne({ where: {email: req.body.email} }).then(user => {
+  login(req, res) {
+    User.findOne({where: {email: req.body.email}}).then(user => {
       if (!user || !passwordHash.verify(req.body.password, user.password)) {
         res.status(401).send('Email or password is not valid');
       } else {
         const token = req.session.token = jwt.sign({
           email: user.email,
           id: user.id
-          }, secret.key);
+        }, secret.key);
         res.json({'token': token});
       }
     })
