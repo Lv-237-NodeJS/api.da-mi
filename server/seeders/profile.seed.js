356a8534d9bf3453dd7d429984242a9f416afa4e
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-06 01:35:47 +0300

    Added some corrected changes to the profile.seed.js

--- server/seeders/profile.seed.js
+++ server/seeders/profile.seed.js
@@ -4,24 +4,24 @@ module.exports = {
   up: (queryInterface, Sequelize) =>
     queryInterface.bulkInsert('Profiles', [
       {
-        first_name: 'John',
-        last_name: 'Doe',
-        birth_date: '1980-06-17',
-        address: 'Street 1',
-        city: 'Chicago',
-        country: 'US',
-        createdAt: '2017-05-30T19:34:26.150Z',
-        updatedAt: '2017-05-30T19:34:26.150Z'
+        first_name: 'Ivan',
+        last_name: 'Yarymovych',
+        birth_date: '1994-07-02',
+        address: 'Sheptyckogo street, 46',
+        city: 'Lviv',
+        country: 'Ukraine',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
       },
       {
-        first_name: 'Tom',
-        last_name: 'Smith',
-        birth_date: '1990-06-17',
-        address: 'Street 2',
-        city: 'London',
-        country: 'UK',
-        createdAt: '2017-05-30T19:34:26.150Z',
-        updatedAt: '2017-05-30T19:34:26.150Z'
+        first_name: 'Roman',
+        last_name: 'Kulyk',
+        birth_date: '1996-06-17',
+        address: 'Nova street, 2',
+        city: 'Novyi Rozdil',
+        country: 'Ukraine',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
       }
     ]),
   down: (queryInterface, Sequelize) =>
