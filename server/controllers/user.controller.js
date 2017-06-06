38a166b2cb35a74b1dc1adf6c99c5a0cfb931467
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-06 14:29:01 +0300

    Deleted hooks from user controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,24 +1,16 @@
 'use strict';
 
 const User = require('../../config/db').User;
-const passwordHash = require('password-hash');
 
 module.exports = {
   create(req, res) {
-    const nowDate = new Date();
     let assignUser = Object.assign({}, req.body);
 
-    User.hook('afterValidate', (user, options) => {
-      user.password = passwordHash.generate(user.password);
-    });
-    User.hook('beforeCreate', (user, options) => {
-      user.createdAt = nowDate.getTime();
-      user.updatedAt = nowDate.getTime();
-    });
     User.create(assignUser)
     .then(user => res.status(201).send(user))
     .catch(error => res.status(400).send(error));
   },
+
   destroy(req, res) {
     User.findById(req.params.id)
     .then(user => {
