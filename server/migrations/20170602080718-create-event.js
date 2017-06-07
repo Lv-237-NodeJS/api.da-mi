937cf8677d42685e6a25c7f86c453baa492e4876
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-08 02:11:06 +0300

    Generated migration for event model

---
+++ server/migrations/20170602080718-create-event.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      date_event: {
+        type: Sequelize.DATE
+      },
+      owner: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      location_name: {
+        type: Sequelize.STRING
+      },
+      longitude: {
+        type: Sequelize.FLOAT
+      },
+      latitude: {
+        type: Sequelize.FLOAT
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      status_event_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'statusEvents',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        type: Sequelize.BIGINT
+      },
+      updatedAt: {
+        type: Sequelize.BIGINT
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Events');
+  }
+};
