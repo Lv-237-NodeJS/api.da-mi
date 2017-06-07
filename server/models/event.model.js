fabc4888b8c3d14805a6e86571b30b4af50bdbc3
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-08 02:05:53 +0300

    Created event model

---
+++ server/models/event.model.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Event = sequelize.define('Event', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date_event: {
+      type: DataTypes.DATE
+    },
+    owner: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    location_name: {
+      type: DataTypes.STRING
+    },
+    longitude: {
+      type: DataTypes.FLOAT
+    },
+    latitude: {
+      type: DataTypes.FLOAT
+    },
+    description: {
+      type: DataTypes.TEXT
+    },
+    status_event_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      defaultValue: 1
+    },
+    createdAt: {
+      type: DataTypes.BIGINT,
+      allowNull: true
+    },
+    updatedAt: {
+      type: DataTypes.BIGINT,
+      allowNull: true
+    }
+  }, {
+    paranoid: false,
+    timestamps: false,
+    classMethods: {
+      associate: models => {
+        Event.hasOne(models.statusEvent, {
+          foreignKey: 'id'
+        });
+      }
+    },
+    hooks: {
+      beforeCreate: (event, options) => {
+        event.createdAt = new Date().getTime();
+        event.updatedAt = new Date().getTime();
+      },
+      beforeUpdate: (event, options) => {
+        event.updatedAt = new Date().getTime();
+      }
+    }
+  });
+  return Event;
+};
