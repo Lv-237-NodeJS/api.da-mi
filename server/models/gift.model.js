bccbf4659768a942b1a56fbde95aef3395350213
Denis <denis.bodnar@gmail.com>
2017-06-11 15:05:12 +0300

    Create gift model

---
+++ server/models/gift.model.js
@@ -0,0 +1,70 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Gift = sequelize.define('Gift', {
+    name: {
+      type: DataTypes.STRING(15),
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    link: {
+      type: DataTypes.STRING(150),
+      allowNull: true
+    },
+    event_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    is_available: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false
+    },
+    status_gift_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.BIGINT,
+      allowNull: true
+    },
+    updatedAt: {
+      type: DataTypes.BIGINT,
+      allowNull: true
+    }
+  }, {
+    paranoid: false,
+    timestamps: false,
+    classMethods: {
+      associate: models => {
+        Gift.hasOne(models.Event, {
+          foreignKey: 'id',
+          onDelete: 'CASCADE',
+          hooks: true
+        });
+        Gift.hasOne(models.GiftStatus, {
+          foreignKey: 'id',
+          onDelete: 'CASCADE',
+          hooks: true
+        });
+        Gift.hasMany(models.Donor, {
+          foreignKey: 'id',
+          hooks: true
+        });
+      }
+    },
+    hooks: {
+      beforeCreate: (gift, options) => {
+        gift.createdAt = new Date().getTime();
+        gift.updatedAt = new Date().getTime();
+      },
+      beforeUpdate: (gift, options) => {
+        gift.updatedAt = new Date().getTime();
+      }
+    }
+  });
+  return Gift;
+};
