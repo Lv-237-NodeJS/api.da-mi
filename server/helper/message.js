8765f6d0eac87c49482b146db48550708447fc33
yuriy <borodatuu@gmail.com>
2017-06-14 00:13:17 +0300

    create message to backend

---
+++ server/helper/message.js
@@ -0,0 +1,9 @@
+const message = {
+  activation: '~*~🎁~* Activation *~🎁~*~',
+  emailUsed: 'That email address is already in use',
+  userError: 'User Not Found',
+  profileError: 'Profile Not Found',
+  userNotFound: 'User has not found. Please try again!'
+};
+
+export default message;
