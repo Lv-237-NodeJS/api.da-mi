b2f5a324c040b63cd4a2ae1e6886519ce0dced13
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-06-18 00:34:15 +0300

    Merde dev

--- server/helper/messages.js
+++ server/helper/messages.js
@@ -2,5 +2,6 @@ module.exports = {
   'linkNotValid': 'Link are not valid or time expired!',
   'userNotFound': 'This user is not found!',
   'linkAlreadyActivated': 'Link has already actived!',
-  'congratulation': 'Congratulation!'
+  'congratulation': 'Congratulation!',
+  'eventNotFound': 'Event has not found! Try again!'
 };
