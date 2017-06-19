72625da776e5f703d10fea2d816b621ed4300bff
yuriy <borodatuu@gmail.com>
2017-06-19 03:26:27 +0300

    add field unique to user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -7,6 +7,7 @@ module.exports  = (sequelize, DataTypes) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         isEmail: true
       }
