218bdd5eba75ee13899e2475d47ba2f073d8036c
yuriy <borodatuu@gmail.com>
2017-06-19 16:20:37 +0300

    change path in route

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,10 +12,8 @@ module.exports = app => {
   app.get('/api/profile/:id', profileController.retrieve);
   app.put('/api/profile/:id', profileController.update);
 
-  app.post('/api/users', userController.create);
-
+  app.post('/api/auth/users', userController.create);
   app.get('/api/user/:id', userController.retrieve);
-
   app.delete('/api/user/:id', userController.destroy);
 
   app.post('/api/events', eventController.create);
