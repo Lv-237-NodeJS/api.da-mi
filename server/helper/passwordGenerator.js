a8ecff071d990e692b49aebf6f8added32766fe4
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-06-20 03:37:50 +0300

    Code refactoring

--- server/helper/passwordGenerator.js
+++ server/helper/passwordGenerator.js
@@ -5,9 +5,8 @@ module.exports = {
     const chars =
       ['qwertyuiopasdfghjklzxcvbnm', 'QWERTYUIOPASDFGHJKLZXCVBNM', '$@$!%*?&', '1234567890'];
     let password = '';
-    let i;
 
-    for (i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       password += chars.map(char => char.charAt(Math.floor(Math.random() * char.length))).join('');
     }
     return password;
