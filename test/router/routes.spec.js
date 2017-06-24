9f8eaf6d2d712f8220791c6de4ea62672eed2004
Ivan Shyika <i.shyyka@ukr.net>
2017-06-24 08:53:45 +0300

    Modified test server

--- test/router/routes.spec.js
+++ test/router/routes.spec.js
@@ -1,33 +1,45 @@
 const server = require('../../server');
-const PORT = parseInt(process.env.PORT, 10) || 8082;
-const baseUrl = 'http://localhost:' + PORT;
-const request = require('request');
+const frisby = require('frisby');
+
+const baseUrl = 'http://localhost:8082';
 
 describe('Hello DAMI API Server', () => {
+
   describe('GET /', () => {
     it('returns status code 200', () => {
-      request.get(baseUrl + '/', (error, response, body) => {
-        expect(response.statusCode).toBe(200);
-      });
+      frisby.create('status code 200')
+        .get(baseUrl + '/')
+        .expectStatus(200)
+        .toss();
     });
     it('should respond with Welcome to the DAMI API!', done => {
-      request(baseUrl + '/', (error, response, body) => {
-        expect(body).toEqual('{"message":"Welcome to the API Da-Mi."}');
-        done();
-      });
+      frisby.create('status code 200')
+        .get(baseUrl + '/')
+        .expectJSON({
+          message: 'Welcome to the API Da-Mi'
+        });
+      done();
     });
   });
+
   describe('GET /api', () => {
     it('returns status code 200', () => {
-      request.get(baseUrl, (error, response, body) => {
-        expect(response.statusCode).toBe(200);
-      });
+      frisby.create('status code 200')
+        .get(baseUrl + '/api')
+        .expectStatus(200)
+        .toss();
     });
     it('should respond with Welcome to the DAMI API!', done => {
-      request(baseUrl + '/api', (error, response, body) => {
-        expect(body).toEqual('{"message":"Welcome to the DAMI API!"}');
-        done();
-      });
+      frisby.create('status code 200')
+        .get(baseUrl + '/')
+        .expectJSON({
+          message: 'Welcome to the API Da-Mi'
+        });
+      done();
     });
   });
 });
+
+setTimeout(() => {
+    server.close();
+  }, 5000);
