59e329fce29cacb60e6f92f12fd0c0d8ca2c3d6d
Ivan Shyika <i.shyyka@ukr.net>
2017-06-24 08:52:36 +0300

    Add test user controller

--- test/specs/sample-spec.js
+++
@@ -1,8 +0,0 @@
-const calculator = require('../../server/specs/sample.js');
-
-describe('multiplication', () => {
-  it('should multiply 2 and 3', () => {
-    let product = calculator.multiply(2, 3);
-    expect(product).toBe(6);
-  });
-});
