9e0a811b5ef4bb095fb3618b5889af44fa8b532d
Denis <denis.bodnar@gmail.com>
2017-06-27 01:33:45 +0300

    Improve controllers code

--- server/controllers/gift.controller.js
+++ server/controllers/gift.controller.js
@@ -6,37 +6,37 @@ const Event = DB.Event;
 const Guest = DB.Guest;
 const { messages } = require('./../helper');
 
-const validateGift = (entity, eventId) => {
-  return entity && entity.dataValues.event_id === Number(eventId);
-};
-
-const isEventOwner = (eventId, userId, entity) => {
+const isEventOwner = (eventId, userId, gift) => {
   return Event.findOne({
     where: {id: Number(eventId),
       $and: {owner: userId}
     }
   })
-  .then(result => result && Number(result.dataValues.id) && validateGift(entity, eventId))
+  .then(event => event && Number(event.dataValues.id) && 
+    (!!gift && gift.dataValues.event_id === Number(eventId)))
   .catch(error => error);
 };
 
-const isEventGuest = (eventId, userId, entity) => {
+const isEventGuest = (eventId, userId, gift, res) => {
   return Guest.findOne({
     where: {event_id: Number(eventId),
       $and: {user_id: userId}
     }
   })
-  .then(result => result && Number(result.dataValues.id) && validateGift(entity, eventId))
+  .then(guest => guest && Number(guest.dataValues.id) && 
+    (!!gift && gift.dataValues.event_id === Number(eventId)) ||
+    res.status(403).send(messages.giftNotFound))
   .catch(error => error);
 };
 
-const eventIsDraft = eventId => {
+const eventIsDraft = (eventId, res) => {
   return Event.findOne({
     where: {id: Number(eventId),
       $and: {status_event_id: 1}
     }
   })
-  .then(result => result && Number(result.dataValues.id))
+  .then(result => result && Number(result.dataValues.id)) ||
+    res.status(403).send(messages.accessDenied)
   .catch(error => error);
 };
 
@@ -46,44 +46,32 @@ module.exports = {
     Gift.create(giftParams)
     .then(gift => isEventOwner(req.params.id, req.decoded.id, gift)
       .then(out => !!out && res.status(201).send(gift) ||
-        res.status(403).send(messages.accessDenied)
-      )
+      res.status(403).send(messages.accessDenied))
     )
-    .catch(error => res.status(400).send(error));
+    .catch(error => res.status(400));
   },
 
   list(req, res) {
-    Gift.findAll({
-      attributes: ['id', 'name', 'description', 'link', 'is_available', 'status'],
-      where: {event_id: req.params.id}
-    })
+    Gift.findAll({where: {event_id: req.params.id}})
     .then(gifts => isEventOwner(req.params.id, req.decoded.id, gifts)
       .then(out => !!out && res.status(200).send(gifts) ||
         isEventGuest(req.params.id, req.decoded.id, gifts)
-        .then(out => !!out &&
-          res.status(200).send(gifts) ||
-          res.status(403).send(messages.accessDenied)
+        .then(out => !!out && res.status(200).send(gifts)
         )
       )
     )
-    .catch(error => res.status(400).send(error));
+    .catch(error => res.status(400));
   },
 
   retrieve(req, res) {
-    Gift.findById(
-      req.params.gift_id, {
-      attributes: ['name', 'description', 'link', 'is_available', 'event_id', 'status']
-    })
+    Gift.findById(req.params.gift_id)
     .then(gift => isEventOwner(req.params.id, req.decoded.id, gift)
-      .then(out => !!out &&
-        res.status(200).send(gift) ||
-        (isEventGuest(req.params.id, req.decoded.id, gift).then(out => !!out &&
-          res.status(200).send(gift) ||
-          res.status(404).send(messages.giftNotFound))
-        )
+      .then(out => !!out && res.status(200).send(gift) ||
+        isEventGuest(req.params.id, req.decoded.id, gift, res)
+          .then(out => !!out && res.status(200).send(gift))
       )
     )
-    .catch(error => res.status(400).send(error));
+    .catch(error => res.status(400));
   },
 
   update(req, res) {
@@ -93,15 +81,12 @@ module.exports = {
           gift.updateAttributes(Object.assign({}, req.body))
           .then(gift => res.status(200).send(gift))
           .catch(error => res.status(400).send(error)) ||
-        (isEventGuest(req.params.id, req.decoded.id, gift).then(out => !!out &&
-            gift.updateAttributes({is_available: req.body.is_available})
-            .then(gift => res.status(200).send(gift))
-            .catch(error => res.status(400).send(error)) ||
-          res.status(404).send(messages.giftNotFound))
-        )
+          gift.updateAttributes({is_available: req.body.is_available})
+          .then(gift => res.status(200).send(gift))
+          .catch(error => res.status(400).send(error))
       )
     )
-    .catch(error => res.status(400).send(error));
+    .catch(error => res.status(400));
   },
 
   destroy(req, res) {
@@ -109,14 +94,13 @@ module.exports = {
     .then(gift =>
       isEventOwner(req.params.id, req.decoded.id, gift)
         .then(out => !!out &&
-          eventIsDraft(req.params.id).then(out => !!out &&
+          eventIsDraft(req.params.id, res).then(out => !!out &&
             gift
             .destroy()
             .then(gift => res.status(204).send(gift))
             .catch(error => res.status(400).send(error))
-          ) || res.status(403).send(messages.accessDenied)
-        )
+        ) || res.status(403).send(messages.accessDenied))
       )
-    .catch(error => res.status(400).send(error));
+    .catch(error => res.status(400));
   }
 };
