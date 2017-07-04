d1e320787cd93aa267c0da58685ace0aea9b4140
yuriy <borodatuu@gmail.com>
2017-07-05 01:19:10 +0300

    create controller to support

---
+++ server/controllers/support.controller.js
@@ -0,0 +1,18 @@
+const { mailer, messages } = require('./../helper');
+
+module.exports = {
+  support(req, res) {
+    let assignSupport = Object.assign({}, req.body);
+    let data = {
+      subject: messages.problem,
+      img: 'error.png',
+      email: 'dami.suppor@gmail.com',
+      firstname: req.body.name,
+      lastname: req.body.surname,
+      mailsForSupport: req.body.email,
+      text: req.body.textarea
+    };
+    mailer(data, 'support');
+    res.status(201).send();
+  }
+};
