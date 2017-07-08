3951408d3eaaf6450814f5f78d5923fa11bf6e1e
yuliannakorol <yuliannakorol@gmail.com>
2017-07-08 16:56:29 +0300

    refactored profile controller

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -14,12 +14,12 @@ module.exports = {
   },
 
   update(req, res) {
-    const updateProfile = Object.assign({}, req.body);
+    // const updateProfile = Object.assign({}, req.body);
 
     Profile.findById(req.params.id)
     .then(profile => {
       profile || res.status(404).json(messages.profileError);
-      profile.updateAttributes(updateProfile)
+      profile.updateAttributes(Object.assign({}, req.body))
       .then(() => res.status(200).send(profile))
       .catch(error => res.status(400).send(error));
     })
