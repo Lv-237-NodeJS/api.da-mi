577d166b57963958f0c400956076c35fb7b8be2e
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-07-08 23:12:32 +0300

    Deleted unneeded attributes property

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -7,7 +7,6 @@ const messages = require('../helper/messages');
 module.exports = {
   create(req, res) {
     let assignEvent = Object.assign({}, req.body, {owner: req.decoded.id});
-
     Event.create(assignEvent)
       .then(event => res.status(201).send(event))
       .catch(error => res.status(400).send(error));
@@ -15,9 +14,6 @@ module.exports = {
 
   list(req, res) {
     Event.findAll({
-      attributes: [
-        'id', 'name', 'date_event', 'location_name', 'longitude', 'latitude', 'description'
-      ],
       where: {owner: req.decoded.id},
       order: [
         ['createdAt', 'DESC']
@@ -28,11 +24,7 @@ module.exports = {
   },
 
   retrieve(req, res) {
-    Event.findById(
-        req.params.id, {
-        attributes: [
-          'name', 'date_event', 'location_name', 'longitude', 'latitude', 'description', 'owner'
-        ]})
+    Event.findById(req.params.id)
       .then(event => {
         if (event.dataValues.owner !== req.decoded.id || !event)  {
           return res.status(400).send(messages.eventNotFound);
@@ -51,7 +43,7 @@ module.exports = {
         return res.status(404).send(messages.eventNotFound);
       }
       let updatedEvent = Object.assign({}, req.body);
-      return event.updateAttributes(updatedEvent)
+      event.updateAttributes(updatedEvent)
       .then(event => res.status(200).send(event))
       .catch(error => res.status(400).send(error));
     })
