a6d26f41170bd3cc4a40ef2cbf98b998ec70641d
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-07-15 15:45:54 +0300

    Deleted empty line

--- server/helper/hooks.js
+++ server/helper/hooks.js
@@ -6,7 +6,6 @@ module.exports = {
   beforeCreate: model => {
     model.date_event && (model.date_event = Date.parse(model.date_event)) ||
       (model.password = passwordHash.generate(model.password));
-      
     model.createdAt = new Date().getTime();
     model.updatedAt = new Date().getTime();
   },
