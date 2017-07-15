09eefcbbaa59fbe69d93cced31d1c12ee76488e7
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-07-15 18:25:03 +0300

    Added hashing password to the beforeUpdate global hook

--- server/helper/hooks.js
+++ server/helper/hooks.js
@@ -4,12 +4,13 @@ const passwordHash = require('password-hash');
 
 module.exports = {
   beforeCreate: model => {
-    model.date_event && (model.date_event = Date.parse(model.date_event)) ||
-      (model.password = passwordHash.generate(model.password));
+    model.date_event && (model.date_event = Date.parse(model.date_event));
+    model.password && (model.password = passwordHash.generate(model.password));
     model.createdAt = new Date().getTime();
     model.updatedAt = new Date().getTime();
   },
   beforeUpdate: model => {
+  	model.password && (model.password = passwordHash.generate(model.password));
     model.updatedAt = new Date().getTime();
   }
 };
