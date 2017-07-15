5697aaa0772e15418c6c98f5d72f7e018c54dd0a
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-07-15 15:37:54 +0300

    Code refactoring in global hooks.

--- server/helper/hooks.js
+++ server/helper/hooks.js
@@ -4,12 +4,9 @@ const passwordHash = require('password-hash');
 
 module.exports = {
   beforeCreate: model => {
-    if (model.date_event) {
-      model.date_event = Date.parse(model.date_event);
-    }
-    if (model.password) {
-      model.password = passwordHash.generate(model.password);
-    }
+    model.date_event && (model.date_event = Date.parse(model.date_event)) ||
+      (model.password = passwordHash.generate(model.password));
+      
     model.createdAt = new Date().getTime();
     model.updatedAt = new Date().getTime();
   },
